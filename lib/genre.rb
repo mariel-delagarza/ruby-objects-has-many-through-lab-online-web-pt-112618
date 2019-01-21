891ada6e54885da96c82d574ee0e6469c3871f57
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-21 23:52:53 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,15 +1,27 @@
 class Genre
-  attr_accessor :name 
-  
+  attr_accessor :name
+
   @@all = []
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
   def initialize(name)
-    @name = name 
-    @@all << self 
+    @name = name
+    @@all << self
   end
+
+  def songs 
+    Songs.all.select do |song|
+      song.genre = self 
+    end 
+  end 
+
+  def artists 
+    songs.map do |song|
+      song.artist
+    end 
+  end 
   
-end 
+end
