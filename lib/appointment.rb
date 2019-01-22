da6b39fe0ae94de5598121e44a5728c2790445c4
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-22 00:52:57 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,17 @@
 class Appointment
-
+  attr_accessor :date, :patient, :doctor 
+  
   @@all = []
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
   def initialize(date, patient, doctor)
-    @date = date 
+    @date = date
     @patient = patient
-    @doctor = doctor 
-    @@all << self 
-  end 
-  
-end 
+    @doctor = doctor
+    @@all << self
+  end
+
+end
