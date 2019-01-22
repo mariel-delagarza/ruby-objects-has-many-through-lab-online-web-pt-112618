cbdfe799e13bf776d34b03681b61369b26e3da71
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-22 00:30:43 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,21 +1,21 @@
-class Doctor 
+class Doctor
 
-  attr_accessor :name 
-  
+  attr_accessor :name
+
   @@all = []
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
   def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
+    @name = name
+    @@all << self
+  end
 
   def new_appointment(patient, date)
     Appointment.new(date, patient, self)
-    
-  end 
-  
-end 
+
+  end
+
+end
